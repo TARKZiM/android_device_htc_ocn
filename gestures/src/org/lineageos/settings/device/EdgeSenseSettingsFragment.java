39a0a43793e2fa8e7afbf1ac7eba38073c10b57f
Golv <lok02@list.ru>
2020-01-22 10:39:22 +0300

    ocn: gestures: Migrate to androidx
    https://github.com/Claymore1297/android_device_htc_hima/commit/5da2c56eef8ef44df1e5a72fbdd2e567aa9d9022

--- gestures/src/org/lineageos/settings/device/EdgeSenseSettingsFragment.java
+++ gestures/src/org/lineageos/settings/device/EdgeSenseSettingsFragment.java
@@ -19,7 +19,7 @@ package org.lineageos.settings.device;
 
 import android.app.ActionBar;
 import android.os.Bundle;
-import android.support.v14.preference.PreferenceFragment;
+import androidx.preference.PreferenceFragment;
 import android.util.Log;
 
 import org.lineageos.settings.device.widget.SeekBarPreference;

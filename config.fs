fbc1f38c0a919b7f590fa2359e3fd50578eeccf7
Golv <lok02@list.ru>
2020-03-14 23:44:26 +0300

    ocn: update config.fs for Q

--- config.fs
+++ config.fs
@@ -1,6 +1,21 @@
 [AID_VENDOR_QTI_DIAG]
 value:2901
 
+[AID_VENDOR_QDSS]
+value:2902
+
+[AID_VENDOR_RFS]
+value:2903
+
+[AID_VENDOR_RFS_SHARED]
+value:2904
+
+[AID_VENDOR_ADPL_ODL]
+value:2905
+
+[AID_VENDOR_QRTR]
+value:2906
+
 [vendor/bin/wcnss_filter]
 mode: 0755
 user: AID_BLUETOOTH
@@ -21,8 +36,8 @@ caps: BLOCK_SUSPEND NET_ADMIN
 
 [system/vendor/bin/hw/android.hardware.bluetooth@1.0-service-qti]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_BLUETOOTH
+group: AID_BLUETOOTH
 caps: BLOCK_SUSPEND NET_ADMIN
 
 [system/bin/cnss-daemon]
@@ -69,8 +84,8 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/imsdatadaemon]
 mode: 0755
-user: AID_SYSTEM
-group: AID_SYSTEM
+user: AID_RADIO
+group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/imsdatadaemon]
@@ -81,27 +96,27 @@ caps: NET_BIND_SERVICE
 
 [vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [system/vendor/bin/ims_rtp_daemon]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
 [vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
-caps: WAKE_ALARM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [system/vendor/bin/imsrcsd]
 mode: 0755
-user: AID_SYSTEM
+user: AID_RADIO
 group: AID_RADIO
-caps: WAKE_ALARM
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
 
 [vendor/bin/cnd]
 mode: 0755
@@ -127,17 +142,53 @@ user:  AID_GPS
 group: AID_GPS
 caps: NET_BIND_SERVICE
 
+[vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
+[system/vendor/bin/loc_launcher]
+mode: 0755
+user:  AID_GPS
+group: AID_GPS
+caps: SETUID SETGID
+
 [vendor/bin/xtwifi-client]
 mode: 0755
 user:  AID_GPS
 group: AID_GPS
-caps: NET_BIND_SERVICE BLOCK_SUSPEND
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
+[vendor/bin/sensors.qti]
+mode: 0755
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: NET_BIND_SERVICE
 
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[carrier/]
+mode: 0771
+user: AID_RADIO
+group: AID_RADIO
+caps: NET_BIND_SERVICE
 
 [bt_firmware/]
 mode: 0771

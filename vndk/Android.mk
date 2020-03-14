e5f4a128735ad908f43d9ea2292c6eb2054bc4bd
Golv <lok02@list.ru>
2020-03-14 22:19:15 +0300

    ocn: vndk-sp: add LOCAL_MODULE_OWNER := google

--- vndk/Android.mk
+++ vndk/Android.mk
@@ -62,6 +62,7 @@ $(foreach lib,$(VNDK_SP_LIBRARIES),\
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := vndk-sp
+LOCAL_MODULE_OWNER := google
 LOCAL_MODULE_TAGS := optional
 LOCAL_REQUIRED_MODULES := $(addsuffix .vndk-sp-gen,$(VNDK_SP_LIBRARIES))
 include $(BUILD_PHONY_PACKAGE)

fc766a2d0e7e900dbb19e19223144767fb129dbd
Golv <lok02@list.ru>
2020-10-13 14:13:59 +0300

    ocn: device.mk: fix missing separator

--- device.mk
+++ device.mk
@@ -277,7 +277,7 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0-java
+    android.hidl.manager@1.0-java \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \

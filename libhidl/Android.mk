5f86a876020da87bf51000fd01515e67d23ece6b
Golv <lok02@list.ru>
2020-10-13 11:51:54 +0300

    ocn: Remove libhwbinder/libhidltransport deps
    Since these were combined into libhidlbase.
    
    Bug: 135686713
    Test: build only (libhwbinder/libhidltransport are empty)
    Change-Id: I640a3f0e0839214523627bf0b59fd5ae9dd2c61c

--- libhidl/Android.mk
+++ libhidl/Android.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_SHARED_LIBRARIES := libhidlbase
 LOCAL_MODULE := android.hidl.base@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
@@ -23,7 +23,7 @@ include $(BUILD_SHARED_LIBRARY)
 
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_SHARED_LIBRARIES := libhidlbase
 LOCAL_MODULE := android.hidl.base@1.0_system
 LOCAL_INSTALLED_MODULE_STEM := android.hidl.base@1.0.so
 LOCAL_MODULE_TAGS := optional
@@ -31,7 +31,7 @@ LOCAL_MODULE_CLASS := SHARED_LIBRARIES
 include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_SHARED_LIBRARIES := libhidltransport
+LOCAL_SHARED_LIBRARIES := libhidlbase
 LOCAL_MODULE := android.hidl.manager@1.0
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES

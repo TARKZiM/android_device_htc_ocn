2bddc606ecbb73e0e69d35d7ade3c223c0dfa539
Golv <lok02@list.ru>
2020-10-23 17:43:11 +0300

    ocn: libshims: add back memset32 for camera_shim
    For some unknown reason, google removed memset32 method in R.
    Our cam-libs need this, so lets add it back.
    
    *https://github.com/TARKZiM/device_htc_oce/commit/7a5b63c8726e7d72f31c481cedcb5482c658296f

--- shims/Android.mk
+++ shims/Android.mk
@@ -1,13 +1,6 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := camera.cpp
-LOCAL_MULTILIB := 32
-LOCAL_MODULE := libcamera_shim
-LOCAL_MODULE_TAGS := optional
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
 LOCAL_SRC_FILES := ims_shimmy.cpp
 LOCAL_MULTILIB := 64
 LOCAL_MODULE := libims_shim

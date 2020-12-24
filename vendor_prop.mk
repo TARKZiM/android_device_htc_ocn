0d9ec677e6cf0f6870e9155b10876c0e95eb7eea
Golv <lok02@list.ru>
2020-12-24 12:18:34 +0300

    ocn: prop: disable ubwc
    * fix green screen on wfd
    
    - https://github.com/crdroidandroid/android_device_xiaomi_vince/commit/b3552411a3ec84f745082f0dbfe08eddc4e55a39

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -125,6 +125,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     vendor.display.disable_rotator_downscale=1 \
     vendor.display.disable_partial_split=1 \
     vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.video.disable.ubwc=1 \
     vendor.display.disable_skip_validate=1
 
 # Factory Reset Protection

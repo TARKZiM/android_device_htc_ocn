953e5cdc1ae10a649edf02566a97b17ce760be68
Golv <lok02@list.ru>
2021-01-19 15:30:22 +0300

    ocn: Disable Blurs by default

--- device.mk
+++ device.mk
@@ -239,6 +239,7 @@ PRODUCT_PACKAGES += \
 # Enable blurs
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.surface_flinger.supports_background_blur=1 \
+    persist.sys.sf.disable_blurs=1 \
     ro.sf.blurs_are_expensive=1
 
 # Fingerprint

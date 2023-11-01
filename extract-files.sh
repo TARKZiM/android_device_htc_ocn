ea10e1d5cb60e9598508b4e0c8a6019615a44402
LuK1337 <priv.luk@gmail.com>
2023-11-01 18:11:13 +0100

    ocn: Use libutils-v33 for pm-service
    Change-Id: Ia998fa72d8747de18010abe6491d2c374cb7aa0a

--- extract-files.sh
+++ extract-files.sh
@@ -78,6 +78,9 @@ function blob_fixup() {
 #    lib64/libwfdnative.so)
 #        "${PATCHELF}" --remove-needed "android.hidl.base@1.0.so" "${2}"
 #        ;;
+    vendor/bin/pm-service)
+        grep -q libutils-v33.so "${2}" || "${PATCHELF}" --add-needed "libutils-v33.so" "${2}"
+    ;;
     esac
 }
 
